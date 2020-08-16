1f9dbf694a13a14b24b7d0d39ab4d8c9194a27f9
Kezia Lopez <keziakl@stanford.edu>
2020-08-15 19:09:49 -0500

    picture update

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,27 @@
-console.log('Here\'s a hidden message');
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+const CHART = document.getElementById("lineChart");
+console.log(CHART);
+let lineChart = new Chart(CHART, {
+    type: 'line'
+    data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75, 192, 192, 0.4)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+        }
+    ]}
+})
+
